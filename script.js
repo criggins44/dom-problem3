0ea5b5d55b9046170bb913702c69e2d7e8c85999
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-20 13:44:22 -0400

    modified the variable names for clarity

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function myFunction(){
 	const vehicle = document.getElementById("mySelect");
 //creates a variable to be used with the length variable
 
-	var x = document.getElementById("mySelect").value;
+	const choice = vehicle.value;
 //identifies the value of the selected item in the mySelect drop down menu
 
-	var y = vehicle.options.length;
+	const totalNumber = vehicle.options.length;
 //returns the number of options used in the drop down menu
 
-	document.getElementById("place").innerHTML = 'There are ' + y + ' options. You chose ' + x;
+	document.getElementById("place").innerHTML = 'There are ' + totalNumber + ' options. You chose ' + choice;
 //creats the string to be shown in the p tag when an option is selected
 }
